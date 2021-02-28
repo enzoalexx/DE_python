f078e84e4b26b13b7064c9a176a79600fbc24d7f
enzoalexx <enzoalexx@gmail.com>
2021-02-28 19:05:18 +0500

    Lesson_8

---
+++ Lesson_8/Task.py
@@ -0,0 +1,212 @@
+import random
+
+
+class Card:
+
+    def __init__(self, name):
+        self._name = name  # Название карточки (для вывода).
+        self._number_of_values = 15  # Количество невычеркнутых полей.
+        self._matrix = []  # Внутреннее представление карточки - список списков.
+        for row in range(0, 3):
+            self._matrix.append([None for column in range(0, 9)])
+
+        # Заполняем карточку нулями на месте будущих номеров. Пустые поля остаются со значением None.
+
+        six_numbers_in_row = True  # Флаг наличия строк с 6 числами. Изначально True для выполнения while.
+
+        # В каждый из первых шести столбцов добавляется по две позиции (значение 0).
+        while six_numbers_in_row:
+            self._clear()
+            for column in range(0, 6):
+                place_for_none = random.randint(0, 2)  # Выбираем позицию, которую оставим нетронутой.
+                for row in range(0, 3):
+                    if row != place_for_none:
+                        self._matrix[row][column] = 0
+
+            six_numbers_in_row = False
+            for row in self._matrix:
+                if row.count(0) == 6:
+                    six_numbers_in_row = True
+                    break
+
+        # В последние три столбца добавляется по одному числу (значение 0).
+        for column in range(6, 9):
+            six_numbers_in_row = True
+            while six_numbers_in_row:
+                place_for_0 = random.randint(0, 2)
+                if self._matrix[place_for_0].count(0) < 5:  # В строку можно добавить число.
+                    six_numbers_in_row = False
+                    self._matrix[place_for_0][column] = 0
+
+        # Перемешиваем столбцы матрицы.
+        for column in range(0, 9):
+            new_position = random.randint(0, 8)
+            for row in range(0, 3):
+                buf = self._matrix[row][column]
+                self._matrix[row][column] = self._matrix[row][new_position]
+                self._matrix[row][new_position] = buf
+
+        # Матрица позиций сгенерирована, заполняем её числами.
+        for row in range(0, 3):
+            for column in range(0, 9):
+                if self._matrix[row][column] == 0:
+                    duplicate_detected = True
+                    while duplicate_detected:
+                        new_value = column * 10 + random.randint(1, 10)
+                        if not [self._matrix[0][column], self._matrix[1][column],
+                                self._matrix[2][column]].count(new_value):
+                            duplicate_detected = False
+                            self._matrix[row][column] = new_value
+
+        # Карточка готова.
+
+    def _clear(self):
+        # Очистка карточки.
+        for row in range(0, 3):
+            for column in range(0, 9):
+                self._matrix[row][column] = None
+
+    @property
+    def is_empty(self):
+        # Возвращает True, если из карточки вычеркнуты все цифры.
+        return not self._number_of_values
+
+    def output(self):
+        # Форматированный вывод карточки с названием.
+        title = ' ' + self._name + ' '
+        if len(title) <= 24:
+            print(title.center(26, '-'))
+        else:
+            print('-' * 26)
+        for y in self._matrix:
+            string = ''
+            for x in y:
+                if not x:
+                    string += '   '
+                elif x == -1:
+                    string += ' - '
+                else:
+                    string += '{:>2} '.format(str(x))
+            print(string)
+        print('-' * 26)
+
+    def find(self, value):
+        # Возвращает True, если в карточке есть число value, иначе возвращает False.
+        for row in self._matrix:
+            if row.count(value):
+                return True
+        return False
+
+    def cross_out(self, value):
+        # Вычёркивает значение value из карточки и возвращает его (вычеркнутый элемент заменяется на -1).
+        # Если такого значения нет, возвращает None.
+        for row in range(0, 3):
+            for column in range(0, 9):
+                if self._matrix[row][column] == value:
+                    self._matrix[row][column] = -1
+                    self._number_of_values -= 1
+                    return value
+        return None
+
+
+class PouchOfBarrels:
+    # Мешочек с бочонками реализовал в виде класса. Знаю, что проще сделать списком,
+    # но мне хотелось поэкспериментировать с итератором.
+
+    def __init__(self):
+        self._array = list(range(1, 91))  # Список с бочонками.
+
+    @property
+    def left(self):
+        # Количество оставшихся в мешке бочонков.
+        return len(self._array)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # Получение каждого нового значения изымает его из списка.
+        if len(self._array):
+            return self._array.pop(random.randint(0, len(self._array) - 1))
+        raise StopIteration
+
+
+last_comp_move = True  # Компьютеру даётся право последнего хода, поэтому возможна ничья (False - ничья невозможна).
+mistake_chance = 0.5  # Шанс (в процентах), что компьютер сделает неправильный ход (0 - компьютер не ошибается).
+
+# Создаём карточки.
+player_card = Card('Ваша карточка')
+comp_card = Card('Карточка компьютера')
+
+# Создаём мешочек с бочонками.
+barrels = PouchOfBarrels()
+
+print('Игра начинается'.upper())
+
+# Установка служебных флагов.
+player_loss = False  # Флаг поражения игрока.
+draw_flag = False  # Флаг ничьи.
+
+for new_barrel in barrels:
+    print('\nНовый бочонок: {} (осталось {})'.format(new_barrel, barrels.left))
+    player_card.output()
+    comp_card.output()
+
+    # Ход игрока.
+    if input('Зачеркнуть цифру? (y/n) ') == 'y':
+        if player_card.cross_out(new_barrel):
+            print('Число {} вычеркнуто из вашей карточки.'.format(new_barrel))
+            if player_card.is_empty and not last_comp_move:
+                # Игрок побеждает, и компьютеру не даётся право последнего хода.
+                break
+        else:
+            print('Будьте внимательнее! Числа {} нет в вашей карточке.'.format(new_barrel))
+            player_loss = True
+            if not last_comp_move:
+                # Игрок ошибается, и компьютеру не даётся право последнего хода.
+                break
+    else:
+        if player_card.find(new_barrel):
+            # Игрок пропустил число.
+            print('Будьте внимательнее! В вашей карточке есть число {}.'.format(new_barrel))
+            player_loss = True
+            if not last_comp_move:
+                # У компьютера нет права последнего хода.
+                break
+
+    # Ход компьютера.
+    if mistake_chance and random.uniform(0, 99) < mistake_chance:
+        # Компьютер совершает ошибку.
+        print('Компьютер ошибается! В его карточке {} {}'.format(
+            'есть число' if comp_card.find(new_barrel) else 'нет числа', new_barrel))
+        if player_loss:
+            # Оба игрока ошиблись - ничья.
+            draw_flag = True
+        # Победитель зависит от значения флагов draw_flag и player_loss.
+        break
+    else:
+        # Компьютер не совершил ошибки.
+        if comp_card.cross_out(new_barrel):
+            print('Компьютер вычёркивает число {} из своей карточки.'.format(new_barrel))
+
+    if player_card.is_empty and comp_card.is_empty:
+        # Если обе карточки пусты - объявляется ничья.
+        draw_flag = True
+        break
+    if player_card.is_empty:
+        # Игрок выиграл.
+        break
+    if comp_card.is_empty:
+        # Игрок проиграл.
+        player_loss = True
+        break
+    if player_loss:
+        # Игрок проиграл, и у компьютера было право последнего хода, но он не ошибся.
+        break
+
+if draw_flag:
+    print('Игра окончена. Ничья!')
+elif player_loss:
+    print('Игра окончена. Вы проиграли!')
+else:
+    print('Игра окончена. Поздравляем, вы выиграли!')
